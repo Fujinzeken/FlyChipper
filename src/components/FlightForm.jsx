746f7ce5993819f6cf13557ef7d674fe414a9fce
micheal <fujinzeken@yahoo.com>
2023-08-17 14:27:15 +0100

    fix:remove unused vars

--- src/components/FlightForm.jsx
+++ src/components/FlightForm.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../styles/flightForm.css";
 import FlightTable from "./FlightTable";
 import { Col, Container, Row } from "reactstrap";
