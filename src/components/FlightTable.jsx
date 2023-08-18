97b22b2287fcde4b15b835620f98ba40350a21b4
micheal <fujinzeken@yahoo.com>
2023-08-18 18:12:50 +0100

    fix:table

--- src/components/FlightTable.jsx
+++ src/components/FlightTable.jsx
@@ -26,61 +26,59 @@ const FlightTable = ({ data, setShowTable }) => {
         <span className="close" onClick={() => setShowTable(false)}>
           X
         </span>
-        <Row>
-          <Col>
-            <table className="table table-bordered">
-              <thead>
-                <tr>
-                  <th>From</th>
-                  <th>To</th>
-                  <th>Price</th>
-                  <th>Avaialable seats</th>
-                  <th>Departure Date</th>
-                  <th>Arrival Date</th>
+
+        <div class="table-responsive-sm">
+          {" "}
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Price</th>
+                <th>Avaialable seats</th>
+                <th>Departure Date</th>
+                <th>Arrival Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {displayPage?.map((item, i) => (
+                <tr key={i}>
+                  <td>{item.cityFrom}</td>
+                  <td>{item.cityTo}</td>
+                  <td>€{item.price}</td>
+                  <td>{item.availability.seats}</td>
+                  <td>
+                    {new Date(item.local_departure).toLocaleDateString(
+                      "en-GB",
+                      {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                      }
+                    )}
+                  </td>
+                  <td>
+                    {new Date(item.local_arrival).toLocaleDateString("en-GB", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    })}
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {displayPage?.map((item, i) => (
-                  <tr key={i}>
-                    <td>{item.cityFrom}</td>
-                    <td>{item.cityTo}</td>
-                    <td>€{item.price}</td>
-                    <td>{item.availability.seats}</td>
-                    <td>
-                      {new Date(item.local_departure).toLocaleDateString(
-                        "en-GB",
-                        {
-                          day: "2-digit",
-                          month: "2-digit",
-                          year: "numeric",
-                        }
-                      )}
-                    </td>
-                    <td>
-                      {new Date(item.local_arrival).toLocaleDateString(
-                        "en-GB",
-                        {
-                          day: "2-digit",
-                          month: "2-digit",
-                          year: "numeric",
-                        }
-                      )}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Col>
-          <div>
-            <ReactPaginate
-              pageCount={pageCount}
-              onPageChange={changePage}
-              previousLabel="Prev"
-              nextLabel="Next"
-              containerClassName="paginationBtns"
-            />
-          </div>
-        </Row>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          <ReactPaginate
+            pageCount={pageCount}
+            onPageChange={changePage}
+            previousLabel="Prev"
+            nextLabel="Next"
+            containerClassName="paginationBtns"
+          />
+        </div>
       </Container>
     </section>
   );
